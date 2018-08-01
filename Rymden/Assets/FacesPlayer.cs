f76e0d92a9787f91508b363f0c39967ec5b302dc
isakALF <40694082+isakALF@users.noreply.github.com>
2018-08-01 15:50:59 +0200

    fiende AI

---
+++ Rymden/Assets/FacesPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacesPlayer : MonoBehaviour {
+
+	public float rotSpeed = 90f;
+
+	Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == null) {
+			//find the players ship
+			GameObject go = GameObject.Find ("PlayerShip");
+
+			if (go != null) {
+				player = go.transform;
+			}
+		}
+
+		if (player == null)
+			return; //try again next frame
+
+		Vector3 dir = player.position - transform.position;
+		dir.Normalize ();
+
+		float zAngle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg + 90;
+
+		Quaternion desiredRot = Quaternion.Euler (0, 0, zAngle);
+
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+	}
+}
