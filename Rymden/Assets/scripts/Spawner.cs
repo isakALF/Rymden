951654f935b43fbf73d52631e54258d24fb9f733
isakALF <40694082+isakALF@users.noreply.github.com>
2018-08-15 16:01:47 +0200

    18 08 15

---
+++ Rymden/Assets/scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Vector3 position;
+
+	public GameObject wave;
+
+	void OnTriggerEnter2D(){
+		Instantiate (wave, position, Quaternion.identity);
+	}
+}
